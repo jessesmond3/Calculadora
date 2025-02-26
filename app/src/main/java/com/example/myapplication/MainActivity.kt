11eb8708c0f39eeff8a6a1475758f93ecf848673
Sareh <al213178@alumnos.uacj.mx>
2025-02-26 08:36:47 -0700

    [AGREGA] una base de la interfaz con diseño sencillo

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,37 +4,91 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import javax.sql.RowSetWriter
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            MyApplicationTheme{
+                Calculadora()
                 }
+            }
+        }
+    }
+
+
+@Composable
+fun Calculadora() {
+    Column (modifier = Modifier.fillMaxSize(),
+    verticalArrangement = Arrangement.Center) {
+        Text(text = "Hello World", modifier = Modifier
+            .padding(10.dp)
+            .fillMaxWidth()
+            .background(Color.Blue)
+            .height(50.dp),
+            textAlign = TextAlign.Right)
+
+        Column(modifier = Modifier.fillMaxSize().background(Color.DarkGray)){
+            Row (horizontalArrangement = Arrangement.SpaceAround,
+                modifier = Modifier.fillMaxWidth()) {
+                Boton("9")
+                Boton("6")
+                Boton("3")
+            }
+
+            Row (horizontalArrangement = Arrangement.SpaceAround,
+                modifier = Modifier.fillMaxWidth()) {
+                Boton("8")
+                Boton("5")
+                Boton("2")
+            }
+            Row (horizontalArrangement = Arrangement.SpaceAround,
+                modifier = Modifier.fillMaxWidth()) {
+                Boton("7")
+                Boton("4")
+                Boton("1")
+            }
+            Row (horizontalArrangement = Arrangement.SpaceAround,
+                modifier = Modifier.fillMaxWidth()) {
+                Boton(".")
+                Boton("0")
+                Boton("op")
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Boton(etiqueta: String){
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        etiqueta, modifier = Modifier.background(Color.Green)
+            //.background(Color.Green)
+            .padding(25.dp)
+            .background(Color.Green)
+            .height(50.dp)
+            .width(50.dp),
+        textAlign = TextAlign.Center,
+        //fontSize = font
     )
 }
 
@@ -42,6 +96,14 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
-        Greeting("Android")
+        Calculadora()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun mostrar_boton(){
+    MyApplicationTheme {
+        Boton("4")
     }
 }
