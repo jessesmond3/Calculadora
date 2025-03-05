78608d11bf4d69439df5e9fadc2426ac12c02410
Sareh <al213178@alumnos.uacj.mx>
2025-03-05 08:57:40 -0700

    [AGREGAR] cambios

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,96 +1,263 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import com.example.myapplication.ui.theme.color_boton
+import com.example.myapplication.ui.theme.color_letra
+import com.example.myapplication.ui.theme.fondo_teclado
+import com.example.myapplication.ui.theme.fondo_teclado
 import javax.sql.RowSetWriter
+
+data class BotonModelo(
+    val id: String,
+    var numero: String,
+    var operacion_aritmetica: OperacionesAritmeticas = OperacionesAritmeticas.Ninguna,
+    var operacion_a_mostrar: String = ""
+) {}
+
+enum class EstadosCalculadora{
+    CuandoEstaEnCero,
+    AgregandoNumeros,
+    SeleccionadoOperacion,
+    MostrandoResultado
+}
+
+enum class OperacionesAritmeticas{
+    Ninguna, // Esta es la opcion por default y sirve para hacer nada
+    Suma,
+    Resta,
+    Multiplicacion,
+    Division,
+    Resultado
+}
+
+var hileras_de_botones_a_dibujar = arrayOf(
+    arrayOf(
+        BotonModelo("boton_9", "9", OperacionesAritmeticas.Multiplicacion, "*"),
+        BotonModelo("boton_8", "8"),
+        BotonModelo("boton_7", "7", OperacionesAritmeticas.Division, "/"),
+    ),
+    arrayOf(
+        BotonModelo("boton_6", "6"),
+        BotonModelo("boton_5", "5", OperacionesAritmeticas.Resultado, "="),
+        BotonModelo("boton_4", "4"),
+    ),
+    arrayOf(
+        BotonModelo("boton_3", "3", OperacionesAritmeticas.Suma, "+"),
+        BotonModelo("boton_2", "2"),
+        BotonModelo("boton_1", "1", OperacionesAritmeticas.Resta, "-"),
+    ),
+    arrayOf(
+        BotonModelo("boton_punto", "."),
+        BotonModelo("boton_0", "0"),
+        BotonModelo("boton_operacion", "OP"),
+    )
+
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MyApplicationTheme{
+            MyApplicationTheme {
                 Calculadora()
+            }
+        }
+    }
+}
+
+@Composable
+fun Calculadora() {
+    var pantalla_calculadora = remember { mutableStateOf("0") }
+    var numero_anterior = remember { mutableStateOf("0") }
+    var estado_de_la_calculadora = remember { mutableStateOf(EstadosCalculadora.CuandoEstaEnCero) }
+    var operacion_seleccionada = remember { mutableStateOf(OperacionesAritmeticas.Ninguna) }
+
+    fun pulsar_boton(boton: BotonModelo){
+        Log.v("BOTONES_INTERFAZ", "Se ha pulsado el boton ${boton.id} de la interfaz")
+        Log.v("OPERACION_SELECCIONADA", "La operacion seleccionada es ${operacion_seleccionada.value}")
+
+        when(estado_de_la_calculadora.value){
+            EstadosCalculadora.CuandoEstaEnCero -> {
+                if(boton.id == "boton_0"){
+                    return
+                }
+                else if(boton.id == "boton_punto"){
+                    pantalla_calculadora.value = pantalla_calculadora.value + boton.numero
+                    return
+                }
+
+                pantalla_calculadora.value = boton.numero
+                estado_de_la_calculadora.value = EstadosCalculadora.AgregandoNumeros
+
+            }
+
+            EstadosCalculadora.AgregandoNumeros -> {
+                if(boton.id == "boton_operacion"){
+                    estado_de_la_calculadora.value = EstadosCalculadora.SeleccionadoOperacion
+                    return
+                }
+
+                pantalla_calculadora.value = pantalla_calculadora.value + boton.numero
+            }
+
+            EstadosCalculadora.SeleccionadoOperacion -> {
+                if(     boton.operacion_aritmetica != OperacionesAritmeticas.Ninguna &&
+                    boton.operacion_aritmetica != OperacionesAritmeticas.Resultado
+                ){
+                    operacion_seleccionada.value = boton.operacion_aritmetica
+                    estado_de_la_calculadora.value = EstadosCalculadora.CuandoEstaEnCero
+
+                    numero_anterior.value = pantalla_calculadora.value
+
+                    pantalla_calculadora.value = "0"
+                    return
+                }
+                // Aqui imprimimos el resultado
+                else if(boton.operacion_aritmetica == OperacionesAritmeticas.Resultado &&
+                    operacion_seleccionada.value != OperacionesAritmeticas.Ninguna){
+
+                    when(operacion_seleccionada.value){
+
+                        OperacionesAritmeticas.Suma -> {
+                            pantalla_calculadora.value = numero_anterior.value + "+" + pantalla_calculadora.value
+                        }
+                        OperacionesAritmeticas.Resta -> {
+                            pantalla_calculadora.value = numero_anterior.value + "-" + pantalla_calculadora.value
+                        }
+                        OperacionesAritmeticas.Multiplicacion -> {
+                            pantalla_calculadora.value = numero_anterior.value + "*" + pantalla_calculadora.value
+                        }
+                        OperacionesAritmeticas.Division -> {
+                            pantalla_calculadora.value = numero_anterior.value + "/" + pantalla_calculadora.value
+                        }
+
+                        else -> {}
+                    }
+
+
+                    estado_de_la_calculadora.value = EstadosCalculadora.MostrandoResultado
+                    return
+                }
+
+                estado_de_la_calculadora.value = EstadosCalculadora.AgregandoNumeros
+            }
+
+
+            EstadosCalculadora.MostrandoResultado -> {
+                numero_anterior.value  = ""
+
+                pantalla_calculadora.value = "0"
+
+                estado_de_la_calculadora.value = EstadosCalculadora.CuandoEstaEnCero
+            }
+        }
+    }
+
+
+    Column(modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = "${pantalla_calculadora.value}", modifier = Modifier
+            .padding(10.dp)
+            .fillMaxWidth()
+            .fillMaxHeight(0.33f)
+            .background(color = Color.White)
+            .height(50.dp),
+            textAlign = TextAlign.Right,
+            color = color_letra,
+            fontSize = 86.sp
+        )
+
+        // Deberia jugar mas con el estilo de aqui
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .background(fondo_teclado)) {
+            for(fila_de_botones in hileras_de_botones_a_dibujar){
+                Row(horizontalArrangement = Arrangement.SpaceAround,
+                    modifier = Modifier.fillMaxWidth()) {
+                    for(boton_a_dibujar in fila_de_botones){
+                        when(estado_de_la_calculadora.value){
+                            EstadosCalculadora.SeleccionadoOperacion -> {
+                                Boton(boton_a_dibujar.operacion_a_mostrar, alPulsar = {
+                                    pulsar_boton(boton_a_dibujar)
+                                })
+                            }
+                            else -> {
+                                Boton(boton_a_dibujar.numero, alPulsar = {
+                                    pulsar_boton(boton_a_dibujar)
+                                })
+                            }
+                        }
+
+                    }
                 }
             }
         }
     }
 
 
+}
+
 @Composable
-fun Calculadora() {
-    Column (modifier = Modifier.fillMaxSize(),
-    verticalArrangement = Arrangement.Center) {
-        Text(text = "Hello World", modifier = Modifier
-            .padding(10.dp)
-            .fillMaxWidth()
-            .background(Color.Blue)
-            .height(50.dp),
-            textAlign = TextAlign.Right)
+fun Boton(etiqueta: String, alPulsar: () -> Unit = {}){
+    Button(onClick = alPulsar, modifier = Modifier
+        .padding(16.dp),
+        colors = ButtonDefaults.buttonColors(containerColor = color_boton),
+        fontSize = 80.sp) {
 
-        Column(modifier = Modifier.fillMaxSize().background(Color.DarkGray)){
-            Row (horizontalArrangement = Arrangement.SpaceAround,
-                modifier = Modifier.fillMaxWidth()) {
-                Boton("9")
-                Boton("6")
-                Boton("3")
-            }
-
-            Row (horizontalArrangement = Arrangement.SpaceAround,
-                modifier = Modifier.fillMaxWidth()) {
-                Boton("8")
-                Boton("5")
-                Boton("2")
-            }
-            Row (horizontalArrangement = Arrangement.SpaceAround,
-                modifier = Modifier.fillMaxWidth()) {
-                Boton("7")
-                Boton("4")
-                Boton("1")
-            }
-            Row (horizontalArrangement = Arrangement.SpaceAround,
-                modifier = Modifier.fillMaxWidth()) {
-                Boton(".")
-                Boton("0")
-                Boton("op")
-            }
-        }
+        Text(
+            etiqueta, modifier = Modifier
+                .background( color = Color.Transparent),
+            textAlign = TextAlign.Center,
+            color = Color.White
+        )
     }
 }
 
-@Composable
-fun Boton(etiqueta: String){
-    Text(
-        etiqueta, modifier = Modifier.background(Color.Green)
-            //.background(Color.Green)
-            .padding(25.dp)
-            .background(Color.Green)
-            .height(50.dp)
-            .width(50.dp),
-        textAlign = TextAlign.Center,
-        //fontSize = font
-    )
-}
+/* NOTAS
+    px:Píxeles: corresponde a los píxeles reales en la pantalla
+    dp:Píxeles independientes de la densidad: una unidad abstracta que se basa en la densidad física de la pantalla.
+    sp : píxeles independientes de la escala: es como la unidad dp, pero la preferencia de tamaño de fuente del usuario también la escala.
+    pt : Puntos – 1/72 de pulgada según el tamaño físico de la pantalla, asumiendo una pantalla con una densidad de 72 ppp.
+    mm : Milímetros – Basado en el tamaño físico de la pantalla.
+    en : Pulgadas – Basado en el tamaño físico de la pantalla.
+*/
 
 @Preview(showBackground = true)
 @Composable
--- app/src/main/java/com/example/myapplication/ui/theme/Color.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val fondo_teclado= Color(0xFFEFA00B)
+val color_letra = Color(0xFF0075C4)
+val color_boton = Color(0xFFD65108)
